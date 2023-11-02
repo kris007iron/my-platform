63a162288013486121d0bc195854d853605b09a8
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2023-11-02 13:13:10 +0100

    samples for frontend

--- front-end/app.js
+++ front-end/app.js
@@ -24,3 +24,26 @@ const get = (url) =>
         .then(response => response.json())
         .catch(error => console.log(error));
 };
+
+const text = document.querySelector('.sec-text');
+
+let lastUpdateTime = new Date();
+
+
+const textOptions = ['Freelancer', 'BackEndDev', 'Programmer'];
+let currentIndex = 1;
+
+const updateText = () =>
+{
+    const currentTime = new Date();
+    const elapsedTime = currentTime - lastUpdateTime;
+
+    if (elapsedTime >= 4000)
+    {
+        text.innerText = textOptions[currentIndex];
+        currentIndex = (currentIndex + 1) % textOptions.length;
+        lastUpdateTime = currentTime;
+    }
+};
+
+setInterval(updateText, 100); // Check every second for updates
