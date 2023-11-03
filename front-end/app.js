2179959e37cd3aac2e22071621a94ca5e9484ea1
kris007iron <kris007.iron@gmail.com>
2023-11-03 21:20:17 +0100

    more styling and getting data from medium

--- front-end/app.js
+++ front-end/app.js
@@ -1,25 +1,19 @@
-const observer = new IntersectionObserver(entries =>
-{
-    entries.forEach((entry) =>
-    {
+const observer = new IntersectionObserver(entries => {
+    entries.forEach((entry) => {
         // console.log(entry);
-        if (entry.isIntersecting)
-        {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else
-        {
+        } else {
             entry.target.classList.remove('show');
         }
     });
 });
 const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((element) =>
-{
+hiddenElements.forEach((element) => {
     observer.observe(element);
 });
 // get request to the server
-const get = (url) =>
-{
+const get = (url) => {
     return fetch(url)
         .then(response => response.json())
         .catch(error => console.log(error));
@@ -33,41 +27,65 @@ let lastUpdateTime = new Date();
 const textOptions = ['Freelancer', 'BackEndDev', 'Programmer'];
 let currentIndex = 1;
 
-const updateText = () =>
-{
+const updateText = () => {
     const currentTime = new Date();
     const elapsedTime = currentTime - lastUpdateTime;
 
-    if (elapsedTime >= 4000)
-    {
+    if (elapsedTime >= 4000) {
         text.innerText = textOptions[currentIndex];
         currentIndex = (currentIndex + 1) % textOptions.length;
         lastUpdateTime = currentTime;
     }
 };
 
-setInterval(updateText, 100); // Check every second for updates
+setInterval(updateText, 10); // Check every second for updates
 
 let data = get('http://localhost:8000/api/v1/projects');
 
-data.then((data) =>
-{
+data.then((data) => {
     console.log(data[0]);
     let projects = document.querySelector('#projects');
     let projectsData = data;
-    projectsData.forEach((project) =>
-    {
-        projects.innerHTML += `
-        <div class="project">
-            <div class="project-img">
-                <img src="${project.images[0]}" alt="">
-            </div>
-            <div class="project-text">
-                <h3>${project.title}</h3>
-                <p>${project.description}</p>
-                <a href="${project.link}"><img src="imgs/githublogo.png"></a>
-            </div>
+    projectsData.forEach((project) => {
+        let tagsHTML = '';
+        for (let i = 0; i < project.tags.length; i++) {
+            tagsHTML += `<span class="tag">${project.tags[i]}</span>`;
+        }
+        projects.innerHTML += `        
+        <a class="project" href="${project.link}">
+        <div class="project-img">
+            <img src="${project.images[0]}" alt="">
         </div>
+        <div class="project-text">
+            <h3>${project.title}</h3>
+            <p>${project.description}</p>
+            <div class="extras">
+            ${tagsHTML}
+            </div>                
+            </div>            
+        </a>
         `;
     });
 });
+let blogs_medium;
+renderBlogs = (blogs) => {
+    if (blogs.items) {
+        return blogs.items.map(post => {
+            return `<div class="column">
+                <div class="card">
+                    <img src=${post.thumbnail} class="Img" />
+                    <h1 class="cardHeader">${post.title}</h1>
+                    <p class="cardText">Posted on: ${post.pubDate}</p>
+                    <a href=${post.link} class="Link"> Read the Full Blog Here!</a>
+                </div>
+            </div>`;
+        })
+    }
+}
+fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@kris007.iron')
+    .then(resp => resp.json())
+    .then(blogs => {
+        blogs_medium = blogs;
+        let blogsDiv = document.querySelector('#posts');
+        blogsDiv.innerHTML = renderBlogs(blogs_medium);
+    });
