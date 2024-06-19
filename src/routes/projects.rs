2feb436a6e8f3a47e43ee771d827b28c8d64780a
Krzysztof Mrugała <66689589+kris007iron@users.noreply.github.com>
2024-06-19 22:56:06 +0200

    chore: Update image path for project uploads

--- src/routes/projects.rs
+++ src/routes/projects.rs
@@ -1,7 +1,10 @@
 extern crate rocket;
 use crate::AuthenticatedUser;
 use mongodb::bson::{doc, Document};
+use rocket::http::Status;
 use rocket::info;
+use rocket::response::status::Custom;
+use rocket::serde::json::Json;
 use rocket::{
     form::Form,
     fs::TempFile,
@@ -42,13 +45,37 @@ pub async fn create_project(
     db: &State<mongodb::Database>,
     mut upload: Form<Upload<'_>>,
     user: AuthenticatedUser,
-) -> Value {
+) -> Result<Json<Value>, Custom<Json<Value>>> {
     let collection: mongodb::Collection<Document> = db.collection("projects");
     let mut image: Vec<String> = Vec::new();
-    let project_path = std::env::current_dir().unwrap();
-    let persist_to_path = project_path.join("src/front-end/imgs/projects/");
+    let project_path = std::env::current_dir().map_err(|e| {
+        (Custom(
+            Status::InternalServerError,
+            Json(json!({"error": e.to_string()})),
+        ));
+    });
+    let persist_to_path = project_path.unwrap().join("src/front-end/imgs/projects/");
     info!("{:?}", persist_to_path);
-    upload.image.persist_to(&persist_to_path).await.unwrap();
-    image.push(upload.image.path().unwrap().to_str().unwrap().to_string());
-    json!({"status": image[0]})
+    if let Err(e) = upload.image.persist_to(&persist_to_path).await {
+        return Err(Custom(
+            Status::InternalServerError,
+            Json(json!({"error": e.to_string()})),
+        ));
+    }
+    let image_path = upload.image.path().ok_or_else(|| {
+        Custom(
+            Status::InternalServerError,
+            Json(json!({"error": "Image path not found"})),
+        )
+    });
+    let image_path = image_path.unwrap();
+    image.push(image_path.to_str().unwrap().to_string());
+    Ok(Json(json!({
+        "title": upload.title.clone(),
+        "description": upload.description.clone(),
+        "link": upload.link.clone(),
+        "image": image,
+        "tags": upload.tags.clone(),
+        "user": user.username,
+    })))
 }
